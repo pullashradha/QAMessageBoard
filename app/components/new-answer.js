b00268a037e4c93d803c4d00ca09fddc1dcca45b
Shradha Pulla <pullashradha@gmail.com>
2016-08-18 22:37:27 -0700

    Create saveQuestion & saveAnswer methods in new-question/answer.js files, work on connecting in index.hbs

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend ({
+  newAnswerForm: false,
+  actions: {
+    model() {
+      this.set("newAnswerForm", true);
+    },
+    saveAnswer(answer) {
+      var params = {
+        answerAuthor: this.get("answerAuthor");
+        answerDate: this.get("answerDate");
+        answer: this.get("answer");
+      };
+      this.set("newAnswerForm", false);
+      this.sendAction(params, "saveAnswer");
+    }
+  }
 });
--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend ({
+  newQuestionForm: false,
+  actions: {
+    model() {
+      this.set("newQuestionForm", true);
+    },
+    saveQuestion(question) {
+      var params = {
+        questionAuthor: this.get("questionAuthor"),
+        questionDate: this.get("questionDate"),
+        question: this.get("question"),
+        notes: this.get("notes")
+      };
+      this.set("newQuestionForm", false);
+      this.sendAction(params, "saveQuestion");
+    }
+  }
 });
