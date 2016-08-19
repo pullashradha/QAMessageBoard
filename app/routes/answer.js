70f65976a833c4296c00571d0ce14b756dbd1edb
Shradha Pulla <pullashradha@gmail.com>
2016-08-18 22:23:01 -0700

    Rearrange properties order in models, create new-question & new-answer hbs

--- app/routes/answer.js
+++ app/routes/answer.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend ({
+  answerAuthor: DS.attr(),
+  answerDate: DS.attr(),
+  answer: DS.attr()
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend ({
+  questionAuthor: DS.attr(),
+  questionDate: DS.attr(),
+  question: DS.attr(),
+  notes: DS.attr()
 });
